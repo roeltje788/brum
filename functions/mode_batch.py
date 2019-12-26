c51958e286f61ed685f8b42a419384292496652f
Raoul <r.h.h.g.m.linssen@student.utwente.nl>
2019-12-26 14:03:48 +0100

    batch zit erin

--- functions/mode_batch.py
+++ functions/mode_batch.py
@@ -1,5 +1,14 @@
-def batch():
-    pass
+def batch_report(file):
 
+    with open(file) as batch:
 
+        for line in batch:
+            generate_report(line)
+
+def batch_lookup(file,workers):
+
+    with open(file) as batch:
+
+        for line in batch:
+            analyse_data(line,workers)
 
