6c5def5c3ac528628065150d5f42a86f080a4039
Raoul <r.h.h.g.m.linssen@student.utwente.nl>
2020-01-11 17:33:32 +0100

    domain lookup werkt

--- functions/mode_domainreport.py
+++ functions/mode_domainreport.py
@@ -2,6 +2,7 @@
 import csv
 import json
 import os
+import math
 
 # Classes
 
@@ -15,6 +16,20 @@ class result_obj:
 
 # Helper functions
 
+def progress(ticker,progression,percent=0, width=40):
+    left = width * percent // 100
+    right = width - left
+    print('\r{}['.format(ticker), '#' * left, ' ' * right, ']',
+          f' {percent:.0f}%',' ','{}'.format(progression),
+          sep='', end='', flush=True)
+
+def update_ticker(ticker):
+
+    if (ticker == '|'):
+        return '-'
+    else:
+        return '|'
+
 def write_results_to_disk(result_dict,input):
 
     base_dir        = os.path.dirname(input)
@@ -22,7 +37,10 @@ def write_results_to_disk(result_dict,input):
     file_name       = file[0]
     file_extension  = file[1]
 
-    output = file_name+'_results.json'
+    output = base_dir+'/'+file_name+'_results.json'
+
+    print('output:{}'.format(output))
+    print('input:{}'.format(input))
 
     os.system('touch {}'.format(output)) # Create file
 
@@ -41,36 +59,71 @@ def generate_report(settings):
 
     # Open files
 
-    input_dict  =   {}
     ref_dict    =   {}
     result_dict =   {}
 
-
     #CSV to json
-    os.system('csvtojson {} > {}.json'.format(input,input))
     os.system('csvtojson {} > {}.json'.format(reference,reference))
 
     #Open file
-    with open(input+'.json') as tmp_file:
-       input_dict = json.load(tmp_file)
-
     with open(reference+'.json') as tmp_file:
        ref_dict = json.load(tmp_file)
 
+    # Get filesize
+
+    with open(settings.input) as tmp:
+        filesize = sum(1 for line in tmp)
+
+    filesize = filesize - 1 #correcting for first line, this line gives the names of the columns
+
+    print ('The file contains {} lines'.format(filesize))
+
     # Cross reference
 
-    for line in input_dict:
-        if line['response_name'] not in result_dict:
-            result_dict['response_name'] = result_obj(line['response_name'])
+    first_line = True
+    header = []
 
-        for cross in ref_dict:
-            if (line['ns_address'] == cross['ns_address']):
-                if( cross['valid_roa'] == 'valid' ):
-                    result_dict['response_name'].protected   += 1
-                else:
-                    result_dict['response_name'].unprotected += 1
+    progression = 0
 
-                continue # We are done exit for loop
+    ticker = '-'
+
+    with open(input) as input_dict:
+        for line in input_dict:
+
+            # Update info for user
+
+            ticker = update_ticker(ticker)
+            progression +=1
+            progression_percentage = math.floor((progression/filesize)*100)
+            progress(ticker,progression,progression_percentage)
+
+            # Calculate cross reference
+
+            line = line.splitlines()
+            list_line = line[0].split(',')
+
+            if(first_line):
+                header = list_line
+                first_line = False
+            else:
+
+                dict_line = dict(zip(header,list_line))
+
+                response_name = dict_line['response_name']
+
+                if response_name not in result_dict:
+                    result_dict[response_name] = result_obj(response_name)
+
+                for cross in ref_dict:
+                    if (dict_line['ns_address'] == cross['ns_address']):
+                        if( cross['valid_roa'] == 'valid' ):
+                            result_dict[response_name].protected   += 1
+                        else:
+                            result_dict[response_name].unprotected += 1
+
+                        continue # We are done exit for loop
+
+    progress(u'\u2713',100) # Generating is done
 
     # Generate results
 
@@ -80,11 +133,12 @@ def generate_report(settings):
     error               =   0
 
     for domain in result_dict:
-        if (domain.protected == 0 and domain.unprotected == 0): # Strange behaviour -> error
+        print('{}'.format(domain))
+        if (result_dict[domain].protected == 0 and result_dict[domain].unprotected == 0): # Strange behaviour -> error
             error+=1
-        elif (domain.protected == 0):   # Complete unprotected
+        elif (result_dict[domain].protected == 0):   # Complete unprotected
             unprotected+=1
-        elif (domain.unprotected == 0): # Complete protected
+        elif (result_dict[domain].unprotected == 0): # Complete protected
             protected+=1
         else:                           # Partially protected
             partial_protected+=1
